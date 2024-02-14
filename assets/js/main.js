feee0d75e8a751319a09d98055e4ab345c8223cd
freetimezp <kraftukrnet@ukr.net>
2024-02-14 21:17:55 +0200

    03. js-portfolio-49-watches. Main section. Slider.

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,3 +25,25 @@ const linkAction = () => {
 
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
+//swiper slider
+const swiperWatches = new Swiper('.home__swiper', {
+    loop: true,
+    spaceBetween: 32,
+    grabCursor: true,
+    effect: 'creative',
+    creativeEffect: {
+        prev: {
+            translate: [-100, 0, -500],
+            rotate: [0, 0, 15],
+            opacity: 0
+        },
+        next: {
+            translate: [100, 0, -500],
+            rotate: [0, 0, -15],
+            opacity: 0
+        },
+    },
+    pagination: {
+        el: '.swiper-pagination'
+    }
+});
